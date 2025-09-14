375df06c2d83afc47ce4cb691d12431f9098f294
Pushkar <pushkarsoni80002@gmail.com>
2025-09-14 12:00:44 +0530

    Update 3_add.py

--- 3_add.py
+++ 3_add.py
@@ -1,12 +1,12 @@
 # This is a simple program to add numbers
-print("This is a simple program to add numbers;")
+print("This is a simple program to add numbers:")
 a = 1
 b = 2
 
 print (a+b)
 
 
-print("This also add two strings;")
+print("This also add two strings:")
 a = "Harry"
 b = "\nCoder"
 
